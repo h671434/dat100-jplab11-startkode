3423e2aaf0f9d9e839284bd735db96a186df0dff
SweetLejo <heldal.leonard@gmail.com>
2022-11-01 14:56:37 +0100

    binary search and quick sort algorithims

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -3,6 +3,8 @@ package no.hvl.dat100.jplab11.oppgave3;
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
 
+import java.util.Arrays;
+
 public class Blogg {
 
 	Innlegg[] inleggtabell;
@@ -26,32 +28,76 @@ public class Blogg {
 		return this.inleggtabell;
 	}
 
-	private void sortering(){
-		for(int i; i < nesteledig; i++){
-			
+	private void quickSort(Innlegg[] array, int lowIndex, int highIndex){
+		if (lowIndex >= highIndex){
+			return;
 		}
+
+		int pivot = array[highIndex].getId();
+		int leftPointer = lowIndex;
+		int rightPointer = highIndex;
+		while(leftPointer < rightPointer){
+			while(array[leftPointer].getId() <= pivot && leftPointer < rightPointer){
+				leftPointer++;
+			}
+			while(array[rightPointer].getId() >= pivot && leftPointer < rightPointer){
+				rightPointer--;
+			}
+			swap(array, rightPointer, leftPointer);
+		}
+		swap(array, leftPointer, highIndex);
+
+		quickSort(array, lowIndex, leftPointer - 1);
+		quickSort(array, leftPointer + 1, highIndex);
 	}
-	
+
+	private void swap(Innlegg[] array, int index1, int index2){
+		Innlegg temp = array[index1];
+		array[index1] = array[index2];
+		array[index2] = temp;
+
+	}
+
+
 	public int finnInnlegg(Innlegg innlegg) {
-
-		boolean found = false;
-		for(int i = 0; i < this.nesteledig && !found; i++){
-
+		if (nesteledig > 2) {
+			quickSort(this.inleggtabell, 0, nesteledig);
 		}
+		int left = 0;
+		int right = nesteledig - 1;
+		while (left <= right) {
+			int mid = left + ((right - left) / 2);
+			if (this.inleggtabell[mid].getId() == innlegg.getId()) {
+				return mid;
+			} else if (innlegg.getId() < this.inleggtabell[mid].getId()) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return -1;
 	}
+
+
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		return finnInnlegg(innlegg) != -1;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return nesteledig < this.inleggtabell.length;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if(!ledigPlass()){
+			return false;
+		}
+		if(finnes(innlegg)){
+			return false;
+		}
+		inleggtabell[nesteledig] = innlegg;
+		nesteledig++;
+		return true;
 	}
 	
 	public String toString() {
